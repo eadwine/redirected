71ddce9a59cd9aad7a351176d986d5402ecc6429
eadwine <edvinas@Edvinass-Mac.local>
2013-12-08 22:04:28 +0200

    .

--- Redirected/Redirected/AboutViewController.h
+++ Redirected/Redirected/AboutViewController.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
 @interface AboutViewController : UIViewController
-@property (strong, nonatomic) IBOutlet UIWebView *youTubeView;
+@property (weak, nonatomic) IBOutlet UIWebView *youTubeView;
 
 @end
