006fb029f18d2b267e583e0ef085f26eb5e028a1
eadwine <edvinas@Edvinass-Mac.local>
2013-12-11 21:49:44 +0200

    entity

--- Redirected/Redirected/AppDelegate.m
+++ Redirected/Redirected/AppDelegate.m
@@ -4,7 +4,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    
     return YES;
 }
 							
